6cf1ec0717b6743b2d3cac1049ce660207312e70
Neos21 <neos21@gmail.com>
2024-11-25 19:31:15 +0900

    Add Favicon

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       '^/public/images': {
         target: 'http://localhost:5000',  // バックエンドの開発用サーバのポートを指定する
         changeOrigin: true
-      },
+      }
     }
   }
 });
